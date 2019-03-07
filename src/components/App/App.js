3f0a1d394b1f4944503a4ec4aba37de99fb34425
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-03-07 23:39:53 +0400

    pass data via props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -49,6 +49,14 @@ class App extends Component {
     });
   };
 
+  onToggleImportant = id => {
+    console.log("Toggle Important ", id);
+  };
+
+  onToggleDone = id => {
+    console.log("Toggle Done ", id);
+  };
+
   render() {
     return (
       <div className="todo-app">
@@ -58,7 +66,12 @@ class App extends Component {
           <ItemStatusFilter />
         </div>
 
-        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+        <TodoList
+          todos={this.state.todoData}
+          onDeleted={this.deleteItem}
+          onToggleImportant={this.onToggleImportant}
+          onToggleDone={this.onToggleDone}
+        />
         <ItemAddForm onItemAdded={this.addItem} />
       </div>
     );
