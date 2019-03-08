f61e547d804a8bf9b2947c10f48c964e0ff18426
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-03-08 18:38:58 +0400

    add a new item using data entry form

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -3,16 +3,32 @@ import React, { Component } from "react";
 import "./ItemAddForm.css";
 
 class ItemAddForm extends Component {
+  state = {
+    label: ""
+  };
+
+  onLabelChange = event => {
+    this.setState({
+      label: event.target.value
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  };
+
   render() {
     return (
-      <div className="item-add-form">
-        <button
-          className="btn btn-outline-secondary"
-          onClick={() => this.props.onItemAdded("Hello world")}
-        >
-          Add Item
-        </button>
-      </div>
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done"
+        />
+        <button className="btn btn-outline-secondary">Add Item</button>
+      </form>
     );
   }
 }
